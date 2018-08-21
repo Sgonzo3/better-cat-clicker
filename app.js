d73c9228158081a921da4ed112b521d641416fc4
Santos Gonzalez <sgonzo3@gmail.com>
2018-08-20 23:16:07 -0400

    feat: catClicker Duel complete

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-let clickCount = 0;
 
-document.querySelector('img').addEventListener('click', function() {
-  clickCount++;
-  document.getElementById('count').innerText = clickCount;
+let cat1 = document.getElementById('cat1');
+let cat1Count = 0;
+
+// set name
+cat1.querySelector('h2').innerText = "Mikey";
+
+// click counter
+cat1.querySelector('img').addEventListener('click', function() {
+  cat1Count++;
+  cat1.querySelector('.count').innerText = cat1Count;
+});
+
+let cat2 = document.getElementById('cat2');
+let cat2Count = 0;
+
+// set name
+cat2.querySelector('h2').innerText = "Miles";
+
+// click counter
+cat2.querySelector('img').addEventListener('click', function() {
+  cat2Count++;
+  cat2.querySelector('.count').innerText = cat2Count;
 });
