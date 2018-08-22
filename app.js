2159ffc9ed8f32a7335f4ff71ddf0502f030a4d4
Santos Gonzalez <sgonzo3@gmail.com>
2018-08-22 16:23:12 -0400

    feat: cat class constructor methods eventlistener

--- app.js
+++ app.js
@@ -1,24 +1,74 @@
+class Cat {
+  constructor(name, imgURL) {
+    this.name = name;
+    this.imgURL = imgURL;
+    this.divHTML = `<div class=${this.name}><div>`;
+    this.asideLiHTML = `<li class="${this.name}">${this.name}</li>`;
+    this.nameHTML = `<h2 class="${this.name}">${this.name}</h2>`;
+    this.imgHTML = `<div class="image-cropper"><img class="${this.name}" src="${this.imgURL}" alt="a picture of ${this.name}"><div>`;
+    this.clickCounterHTML = `<div>
+      <span>Click Count: </span>
+      <span class="${this.name} counter">0</span>
+    </div>`;
+  }
 
-let cat1 = document.getElementById('cat1');
-let cat1Count = 0;
+  createDiv() {
+    document.body.innerHTML += this.divHTML;
+  }
 
-// set name
-cat1.querySelector('h2').innerText = "Mikey";
+  createAside() {
+    document.querySelector('ul.available-cats').innerHTML += this.asideLiHTML;
+  }
 
-// click counter
-cat1.querySelector('img').addEventListener('click', function() {
-  cat1Count++;
-  cat1.querySelector('.count').innerText = cat1Count;
-});
+  createName() {
+    document.querySelector(`div.${this.name}`).innerHTML += this.nameHTML;
+  }
 
-let cat2 = document.getElementById('cat2');
-let cat2Count = 0;
+  createImg() {
+    document.querySelector(`div.${this.name}`).innerHTML += this.imgHTML;
+  }
 
-// set name
-cat2.querySelector('h2').innerText = "Miles";
+  createClickCounterHTMl() {
+    document.querySelector(`div.${this.name}`).innerHTML += this.clickCounterHTML;
+  }
 
-// click counter
-cat2.querySelector('img').addEventListener('click', function() {
-  cat2Count++;
-  cat2.querySelector('.count').innerText = cat2Count;
-});
+  clickCounter() {
+    this.count = document.querySelector(`span.${this.name}`).innerText;
+    this.count++;
+    document.querySelector(`span.${this.name}`).innerText = this.count;
+  }
+}
+
+let allCats = [];
+
+const cat1 = new Cat("Otto", "images/otto.jpg");
+const cat2 = new Cat("Niko", "images/niko.jpg");
+const cat3 = new Cat("Derp", "images/derp.jpg");
+const cat4 = new Cat("Misty", "images/misty.jpg");
+const cat5 = new Cat("Dirtbags", "images/dirtbags.jpg")
+
+allCats.push(cat1, cat2, cat3, cat4, cat5);
+
+for (let cat of allCats) {
+  cat.createDiv();
+  cat.createAside();
+  cat.createName();
+  cat.createImg();
+  cat.createClickCounterHTMl();
+}
+
+// unique counter on each picture
+for (let cat of allCats) {
+  cat.img = document.querySelector(`img.${cat.name}`);
+  cat.img.addEventListener('click', function() {
+    cat.clickCounter();
+  });
+}
+
+// single uiversal counter for whicever cat is showing
+// will need way to ignore hidden cat elements
+// let count =  document.querySelector('span.counter').innerText;
+// document.querySelector(`img`).addEventListener('click', function() {
+//   count++;
+//   document.querySelector('span.counter').innerText = count;
+// });
